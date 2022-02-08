0f2554e338fda9c2ccd390c068feddc1863f6d40
TheGiantPotato <30511566+TheGiantPotato@users.noreply.github.com>
2022-02-08 15:17:24 +0100

    Added basic random tile generation

---
+++ GXPEngine/Level.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GXPEngine;
+class Level : GameObject
+{
+    List<Tile> levelTiles = new List<Tile>();
+    int tileWidth = 32;
+    int tileHeight = 32;
+
+    public Level()
+    {
+        for(int thisX = 0; thisX < game.width; thisX += tileWidth)
+        {
+            for(int thisY = 0; thisY < game.height; thisY += tileHeight)
+            {
+                int randomNumber = Utils.Random(0, 25);
+                if (randomNumber == 25) --randomNumber;
+                Tile thisTile = new Tile(randomNumber);
+                thisTile.x = thisX;
+                thisTile.y = thisY;
+                levelTiles.Add(thisTile);
+                AddChild(thisTile);
+                Console.Write(randomNumber + "   ");
+            }
+            Console.Write('\n');
+        }
+    }
+
+}
---
+++ GXPEngine/Tile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GXPEngine;
+class Tile : AnimationSprite
+{
+    public Tile(int tileID) : base("nethertileset.png", 5, 5)
+    {
+        
+        SetFrame(tileID);
+
+    }
+}
